8d3c862fe7069804429d7c9cdac572f265a451ae
ErickGaona <Erickgaona625@gmail.com>
2025-04-17 10:45:11 -0500

    Intentando correr app en el puerto 5000

--- app.py
+++ app.py
@@ -56,5 +56,8 @@ def home():
 
     return render_template_string(html, personas=personas)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
